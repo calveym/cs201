2e54c4676824a5c452fd1f85b58948739b705038
LMCMLJ <mjcalvey2@gmail.com>
2017-09-28 14:23:24 -0400

    hw3 complete

--- hw3/Josephus.java
+++ hw3/Josephus.java
@@ -1,4 +1,74 @@
+// Josephus.java
+// Michael Calvey
+// CS201 HW3 Assignment 2
+
 public class Josephus
 {
-    
+  // instance variables
+  Circular circle;
+  int skipNum;
+
+  
+  // constructor
+
+  public Josephus(int numPeople, int numToSkip)
+  // initialises circle with numPeople, saves skipNum
+  {
+    circle = new CircularVector();
+    for(int i = 0; i < numPeople; i++)
+    {
+      circle.addAfterCurrent(i);
+    }
+    skipNum = numToSkip; 
+  }
+
+  
+  // instance methods
+
+  public void findMessenger()
+  // controls skipping and removing until messenger is found
+  {
+    while(circle.size() != 1)
+    {
+      skip();
+      remove();
+    }
+    log("The messenger is " + circle.getCurrent());
+  }
+
+  void skip()
+  // skips through circular vector by skipNum times
+  {
+    for(int i = 0; i < skipNum; i++)
+    {
+      log("Skip " + circle.getCurrent());
+      circle.next();
+    }
+  }
+
+  void remove()
+  // removes current item
+  {
+    log("Remove " + circle.getCurrent());
+    circle.removeCurrent();
+  }
+
+  public void log(String s)
+  // debug logging wrapper
+  {
+    System.out.println(s);
+  }
+
+  public static void main(String[] args)
+  {
+    if(args.length == 0)
+    {
+      System.out.println("usage: Josephus <numPeople> <numToSkip>");
+      return;
+    }
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    Josephus joe = new Josephus(a, b);
+    joe.findMessenger();
+  }
 }
