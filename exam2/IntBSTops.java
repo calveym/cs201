994415898a01dcd275602e40ebc405f8b2beb086
LMCMLJ <mjcalvey2@gmail.com>
2017-11-27 08:35:52 -0500

    Saving progress- almost done

--- exam2/IntBSTops.java
+++ exam2/IntBSTops.java
@@ -3,47 +3,46 @@
 // for CS 201 Exam 2
 //
 // Name: Michael Calvey
+import java.util.Stack;
 
 public class IntBSTops {
 
     // problem 1 --------------------------------------------------
 
-    // main func
-    public static void main(String[] args) {
-        IntBST t = new IntBST(6);
-        t.add(4);
-        t.add(5);
-        t.add(8);
-        t.add(7);
-        t.add(9);
-        p(Integer.toString(leftWidth(t)));
-    }
-
-    // debugging method
-    public static void p(String s) {
-        System.out.println(s);
+    public static void levelOrderWrite(IntBST t) {
+    // prints out the contents of the tree t in level order
+        Stack<IntBST> q = new Stack<IntBST>();
+        q.add(t);
+        while (!q.empty()) {
+            IntBST node = q.pop();
+            if (node != null) {
+                // breadth-first root, left, right
+                System.out.print("  " + node.value());
+                q.push(node.left());
+                q.push(node.right());
+            }
+        }
+        System.out.println();
     }
 
     // returns height of t  (==1 if t is leaf!)
     public static int height(IntBST t) {
-        if(t == null)
+        if(t == null) // base recursion case
             return 0;
-        if(t.isLeaf())
-            return 1;
         if(t.left() != null && t.right() != null) {
+            // left and right nodes exist
             if(height(t.left()) >= height(t.right()))
                 return 1 + height(t.left());
             else if(height(t.right()) > height(t.left()))
                 return 1 + height(t.right());
             else return 0;
-        }
-        else if(t.left() != null && t.right() == null) {
+        } else if(t.left() != null && t.right() == null) {
+            // only left nodes exist
             return 1 + height(t.left());
-        }
-        else if(t.right() != null && t.left() == null) {
+        } else if(t.right() != null && t.left() == null) {
+            // only right nodes exist
             return 1 + height(t.right());
-        }
-        else return 0;
+        } else return 0;
     }
 
     // returns balance factor of tree t
@@ -53,16 +52,15 @@ public class IntBSTops {
 
     // returns true if the tree is balanced (balanced == |balanceFact| <= 1)
     public static boolean isBalanced(IntBST t) {
-        if(t == null)
+        if(t == null) // base recursion case
             return true;
-        if(t.isLeaf())
+        if(t.isLeaf()) // leaf always balanced
             return true;
-        else if (balanceFact(t) > 1 || balanceFact(t) < -1)
+        else if (balanceFact(t) > 1 || balanceFact(t) < -1) // break if imbalanced
             return false;
-        else if(t.left() != null && t.right() != null)
+        else if(t.left() != null && t.right() != null) // check recursively
             return (isBalanced(t.left()) &&  isBalanced(t.right()));
-        else
-            return true; // just a placeholder
+        else return false;
     }
 
     // problem 2 --------------------------------------------------
@@ -77,16 +75,12 @@ public class IntBSTops {
 
         // subtrees done, balance current node
         if(balanceFact(t) > 1) {
-            // if(t.right() == null || t.left() == null)
-            //     return;
             if(balanceFact(t.right()) <= -1)
                 t.right().rotateRight();
-            p("Gets here boi");
             t.rotateLeft();
         } else if (balanceFact(t) < -1) {
             if(balanceFact(t.left()) >= 1)
                 t.left().rotateLeft();
-            p("Gets here fam less than one");
             t.rotateRight();
         }
     }
@@ -96,21 +90,16 @@ public class IntBSTops {
 
     public static int width(IntBST t) {
         // returns width of t:
-        // 0 if t is null
-        // 2 if t is a leaf
-        // if one child is null, 1 + width of the other child
-        // otherwise, the sum of the widths of t's subtrees
-
-        if(t == null)
+        if(t == null)        // 0 if t is null
             return 0;
-        if(t.isLeaf())
+        if(t.isLeaf())       // 2 if t is a leaf
             return 2;
-        if(t.right() != null && t.left() == null)
+        if(t.right() != null && t.left() == null)  // if one child is null, 1 + width of the other child
             return 1 + width(t.right());
         if(t.left() != null && t.right() == null)
             return 1 + width(t.left());
-        else
-            return width(t.right()) + width(t.left());
+        else         // otherwise, the sum of the widths of t's subtrees
+            return width(t.left()) + width(t.right());
     }
 
     public static int leftWidth(IntBST t) {
@@ -121,7 +110,7 @@ public class IntBSTops {
             return 0;
         if(t.isLeaf())
             return 1;
-        else return 1 + leftWidth(t.left());
+        else return width(t.left());
     }
 
     public static int rightWidth(IntBST t) {
@@ -132,36 +121,38 @@ public class IntBSTops {
             return 0;
         if(t.isLeaf())
             return 1;
-        else return 1 + rightWidth(t.right());
+        else return width(t.right());
     }
 
     // problem 4 --------------------------------------------------
 
+    // returns smallest value
     public static IntBST smallest(IntBST t) {
-        // returns first node in t
-
-        // FILL IN for problem 4
-
-        return null; // just a placeholder
+        IntBST pos = t;
+        while(pos.left() != null) // follow pointers until smallest reached
+            pos = pos.left();
+        return pos;
     }
 
+    // returns largest value
     public static IntBST largest(IntBST t) {
-        // returns last node in t
-
-        // FILL IN for problem 4
-
-        return null; // just a placeholder
+        IntBST pos = t;
+        while(pos.right() != null) // follow pointers until largest reached
+            pos = pos.right();
+        return pos;
     }
 
     // problem 5 --------------------------------------------------
 
     public static IntBST next(IntBST t) {
-        // returns next node after current according to in-order traversal
-        // or null if no more
-
-        // FILL IN for problem 5
-
-        return null; // just a placeholder
+        if(t.right() != null) // try right first
+            return smallest(t.right()); // if so return smallest of right node
+        else { // else go up until next reached
+            IntBST pos = t;
+            while(pos.isRightChild())
+                pos = pos.parent();
+            return pos.parent();
+        }
     }
 
     // -------------------------------------------------------------
